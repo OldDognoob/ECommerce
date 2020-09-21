2188db0faf58d4fad3d84cdd2b45fa90592f57f8
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-21 21:07:52 +0300

    wip

--- commerce-app/src/App.js
+++ commerce-app/src/App.js
@@ -1,22 +1,23 @@
-//react
-import React from "react";
-//redux
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-//components
-import Header from "./components/Header";
-import Section from "./components/Section";
-import {DataProvider} from "./components/Context";
+import React from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './components/Header'
+import Section from './components/Section'
+import {DataProvider} from './components/Context'
 
-function App(){
-  return (
-    <DataProvider>
-      <div className="app">
-        <Router>
-          <Header/>
-          <Section/>
-        </Router>
-      </div>
-    </DataProvider>
-  );
+
+class App extends React.Component{
+  render(){
+    return(
+      <DataProvider>
+        <div className="app">
+          <Router>
+            <Header />
+            <Section />
+          </Router>
+        </div>
+      </DataProvider>
+    );
+  }
 }
+
 export default App;
