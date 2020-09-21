107be940498790d5f8ae4800ba0546766b13c3b8
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-09-21 19:11:14 +0300

    wip-app

--- commerce-app/src/App.js
+++ commerce-app/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//react
+import React from "react";
+//redux
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+//components
+import Header from "./components/Header";
+import Section from "./components/Section";
+import {DataProvider} from "./components/Context";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <div className="app">
+        <Router>
+          <Header/>
+          <Section/>
+        </Router>
+      </div>
+    </DataProvider>
   );
 }
-
 export default App;
